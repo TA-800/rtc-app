076ad19c2d04e424dec56342e72f898051ba058e
TA-800 <taherali0905@gmail.com>
2023-03-26 02:49:36 -0400

    create room (FE + BE)

--- app/create_room/page.tsx
+++ app/create_room/page.tsx
@@ -1,12 +1,66 @@
+"use client";
+
 import { Input, TextArea } from "@/utils/comps";
+import supabase from "@/utils/supabase";
+import useUser from "@/utils/useUser";
+import { useRouter } from "next/navigation";
 
 export default function CreateNew() {
+    const router = useRouter();
+    const user = useUser();
+
+    const handleNewRoom = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const target = e.target as typeof e.target & {
+            name: { value: string };
+            description: { value: string };
+        };
+        if (!target.name.value || !target.description.value) return;
+        // Send message to backend
+        supabase
+            .from("rooms")
+            .insert({
+                name: target.name.value,
+                description: target.description.value,
+            })
+            .select()
+            .then(({ data, error }) => {
+                if (error) {
+                    console.log("%cError creating room", "color: red; font-weight: bold; font-size: 1.5rem;");
+                    console.log(error);
+                }
+                if (data) {
+                    // Redirect to the new room
+                    console.log("%cRoom created!", "color: green; font-weight: bold; font-size: 1.5rem;");
+
+                    // Add the new user to the room
+                    supabase
+                        .from("rooms_users")
+                        .insert({
+                            room_id: data[0].id,
+                            user_id: user!.id,
+                        })
+                        .select()
+                        .then(({ data, error }) => {
+                            if (error) {
+                                console.log("%cError adding user to room", "color: red; font-weight: bold; font-size: 1.5rem;");
+                                console.log(error);
+                            }
+                            if (data) {
+                                console.log("%cUser added to room!", "color: green; font-weight: bold; font-size: 1.5rem;");
+                                router.push(`/${data[0].room_id}`);
+                            }
+                        });
+                }
+            });
+    };
+
     // Name and description of the room
     return (
         <>
             <h1>Create a new room</h1>
             <br />
-            <form className="flex flex-col gap-2">
+            <form className="flex flex-col gap-2" onSubmit={(e) => handleNewRoom(e)}>
                 <fieldset className="flex flex-col gap-1">
                     <label htmlFor="name">Name</label>
                     <Input name="name" placeholder="What do you want to call your room?" />
